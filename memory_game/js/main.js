5ae13f4d35ec673803f051a59d1b082c28b30d94
daryl <test@gmail.com>
2019-08-23 00:24:35 +0800

    created game logic part2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
 console.log("Up and running");
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
+
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+var checkCards = cardsInPlay.length;
+if (checkCards === 2) {
+	console.log("Correct"); 
+	} else {
+		console.log("Pick Again");
+	}
+
+var equalityCheck;
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, please try again.")
+}
